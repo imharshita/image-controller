f8aab3f013115a6fb441f03273849cd978e68cc2
harshita <harshita.sharma1729@gmail.com>
2021-03-02 00:17:54 +0530

    adding images test package

--- pkg/images/images_test.go
+++ pkg/images/images_test.go
@@ -7,8 +7,13 @@ import (
 )
 
 func TestProcess(t *testing.T) {
-	result, _ := images.Process("harshitadocker/nginx:1.14.2")
-	if result != "backupregistry/nginx:1.14.2" {
-		t.Errorf("Process(\"harshitadocker/nginx:1.14.2\") failed, expected %v, got %v", "backupregistry/nginx:1.14.2", result)
+	result1, err := images.Process("busybox")
+	if result1 != "backupregistry/busybox" {
+		t.Errorf("Process(\"busybox\") failed, expected %v, got %v  error %v  ", "backupregistry/busybox", result1, err)
+	}
+
+	result2, err := images.Process("nginx:1.14.2")
+	if result2 != "backupregistry/nginx:1.14.2" {
+		t.Errorf("Process(\"nginx:1.14.2\") failed, expected %v, got %v  error %v", "backupregistry/nginx:1.14.2", result2, err)
 	}
 }
